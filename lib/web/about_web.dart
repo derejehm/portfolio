0e58440b942d4aeb4018ad7aad9719d5ecfb75b3
dereje <hailemariam013@gmail.com>
2025-02-25 15:51:08 +0300

    About me web complted.

--- lib/web/about_web.dart
+++ lib/web/about_web.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/components.dart';
 
 class AboutWeb extends StatefulWidget {
   const AboutWeb({super.key});
@@ -10,6 +11,237 @@ class AboutWeb extends StatefulWidget {
 class _AboutWebState extends State<AboutWeb> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var widthDevice = MediaQuery.of(context).size.width;
+    return Scaffold(
+      drawer: DrawerWeb(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Row(
+          children: [
+            Spacer(flex: 3),
+            TabsWeb(title: "Home", route: "/"),
+            Spacer(),
+            TabsWeb(title: "Works", route: "/works"),
+            Spacer(),
+            TabsWeb(title: "Blog", route: "/blog"),
+            Spacer(),
+            TabsWeb(title: "About", route: "/about"),
+            Spacer(),
+            TabsWeb(title: "Contact", route: "/contact"),
+            Spacer(),
+          ],
+        ),
+        iconTheme: IconThemeData(size: 25.0, color: Colors.black),
+      ),
+      body: ListView(
+        children: [
+          //About me first page
+          SizedBox(
+            height: 500.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SanBold("About me", 40.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Hello! I'm Dereje Hailemariam I specialized in flutter development.",
+                      15.0,
+                    ),
+                    Sans(
+                      "I strive to ensure astounding performance  with  state of  ",
+                      15.0,
+                    ),
+                    Sans(
+                      "the art security for Android, Ios, Web, Mac, Linux and Windows",
+                      15.0,
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.greenAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Flutter", 15.0),
+                        ),
+                        SizedBox(width: 7.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.greenAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Firebase", 15.0),
+                        ),
+                        SizedBox(width: 7.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.greenAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Android", 15.0),
+                        ),
+                        SizedBox(width: 7.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.greenAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Ios", 15.0),
+                        ),
+                        SizedBox(width: 7.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.greenAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Mac", 15.0),
+                        ),
+                        SizedBox(width: 7.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.greenAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Windows", 15.0),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 147.0,
+                  backgroundColor: Colors.greenAccent,
+                  child: CircleAvatar(
+                    radius: 143.0,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 140.0,
+                      child: Image.asset(
+                        "assets/derejepic_circle.png",
+                        filterQuality: FilterQuality.high,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //Web development second page
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              AnimatedCard(
+                imagePath: "assets/webL.png",
+                height: 250.0,
+                width: 250.0,
+              ),
+              SizedBox(
+                width: widthDevice / 3,
+                child: Column(
+                  children: [
+                    SanBold("Web development", 30.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "I'm here to build your presence online with state of art web apps.",
+                      15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15.0),
+          //App development third section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                width: widthDevice / 3,
+                child: Column(
+                  children: [
+                    SanBold("App development", 30.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Do you need a high-performance, responsive and beautiful app? Don't worry, I've  got you covered .",
+                      15.0,
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedCard(
+                imagePath: "assets/app.png",
+                height: 250.0,
+                width: 250.0,
+                reverse: true,
+              ),
+            ],
+          ),
+          SizedBox(height: 15.0),
+          //Back-end development forth section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              AnimatedCard(
+                imagePath: "assets/firebase.png",
+                height: 250.0,
+                width: 250.0,
+              ),
+              SizedBox(
+                width: widthDevice / 3,
+                child: Column(
+                  children: [
+                    SanBold("Back-end development", 30.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Do you want your back-end to be highly  scalable and secure? Let's have a conversation on how I can help you with that.",
+                      15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40.0),
+        ],
+      ),
+    );
   }
 }
